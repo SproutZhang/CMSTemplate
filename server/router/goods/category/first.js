876d807829828e933b27330dc53fad85c2fbdf17
张梦雅 <835308232@qq.com>
2019-07-23 14:19:50 +0800

    一级商品的修改和删除接口

--- server/router/goods/category/first.js
+++ server/router/goods/category/first.js
@@ -18,7 +18,44 @@ router.get('/add',(req,res) =>{
 
 //获取一级商品
 router.get('/get',(req,res) =>{
-    res.json({code:0,msg: sql.goodsCategory.first })
+    let { id } = req.query;
+    let data = sql.goodsCategory.first.data;
+    if(id){
+        let fdata = data.filter(item=> item.id == id)
+        res.json({code:0,data: fdata })
+    }else{
+        res.json({code:0,msg: sql.goodsCategory.first })
+    }
+
+})
+//修改名称
+router.get('/edit',(req,res) =>{
+    let { id,cname } = req.query;
+    let data = sql.goodsCategory.first.data;
+    if(id&&cname){
+        data.forEach(item =>{
+            if(item.id == id){
+                item.cname = cname;
+            }
+        })
+        res.json({code:0,msg:'修改成功' })
+    }else{
+        res.json({code:1,msg: '参数错误' })
+    }
+})
+//删除
+router.get('/del',(req,res) =>{
+    let { id } = req.query;
+    let data = sql.goodsCategory.first.data;
+    if(id){
+        let index = data.findIndex(item =>(
+            item.id == id
+        ))
+        data.splice(index,1)
+        res.json({code:0,msg:'删除成功' })
+    }else{
+        res.json({code:1,msg: '参数错误' })
+    }
 })
 
 module.exports = router;
